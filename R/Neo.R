4720b8191c612ad8e420e4e17249ed2f931fb64b
pdiakumis <peterdiakumis@gmail.com>
2026-05-01 13:55:49 +1000

    neo: modify schema, simplify parsing

--- R/Neo.R
+++ R/Neo.R
@@ -25,30 +25,6 @@ Neo <- R6::R6Class(
     #' Tibble of files from [nemo::list_files_dir()].
     initialize = function(path = NULL, files_tbl = NULL) {
       super$initialize(name = "neo", pkg = pkg_name, path = path, files_tbl = files_tbl)
-    },
-    #' @description Read `neo_data.tsv` file.
-    #' @param x (`character(1)`)\cr
-    #' Path to file.
-    parse_candidates = function(x) {
-      self$.parse_file(x, "candidates")
-    },
-    #' @description Tidy `neo_data.tsv` file.
-    #' @param x (`character(1)`)\cr
-    #' Path to file.
-    tidy_candidates = function(x) {
-      self$.tidy_file(x, "candidates")
-    },
-    #' @description Read `neoepitope.tsv` file.
-    #' @param x (`character(1)`)\cr
-    #' Path to file.
-    parse_predictions = function(x) {
-      self$.parse_file(x, "predictions")
-    },
-    #' @description Tidy `neoepitope.tsv` file.
-    #' @param x (`character(1)`)\cr
-    #' Path to file.
-    tidy_predictions = function(x) {
-      self$.tidy_file(x, "predictions")
     }
   )
 )
