afc08d461104a2cae01a3d0e3b1e27ee2ba87b08
pdiakumis <peterdiakumis@gmail.com>
2026-05-12 19:10:09 +1000

    Wigits: re-add Esvee; use metapkg

--- R/Wigits.R
+++ R/Wigits.R
@@ -3,28 +3,24 @@
 #' @description
 #' WiGiTS file parsing and manipulation.
 #' @examples
-#' \dontrun{
-#' path <- here::here("nogit/oa_v2")
+#' path <- system.file("extdata/oa", package = "tidywigits")
 #' w <- Wigits$new(path)
-#' x <-
-#'   w$nemofy(
-#'     diro = "nogit/test_data",
-#'     format = "parquet",
-#'     input_id = "run1"
-#' )
-#' dbconn <- DBI::dbConnect(
-#'   drv = RPostgres::Postgres(),
-#'   dbname = "nemo",
-#'   user = "orcabus"
-#' )
-#' x <-
-#'   w$nemofy(
-#'     format = "db",
-#'     input_id = "runABC456",
-#'     dbconn = dbconn
-#' )
-#' DBI::dbDisconnect(dbconn)
-#' }
+#' dir1 <- tempdir()
+#' #w$tidy()
+#' #w$write(diro = dir1, format = "tsv", input_id = "input1", output_id = "out1")
+#' x <- w$nemofy(diro = file.path(dir1, "out1"), format = "parquet", input_id = "run1")
+#' #dbconn <- DBI::dbConnect(
+#' #  drv = RPostgres::Postgres(),
+#' #  dbname = "nemo",
+#' #  user = "orcabus"
+#' #)
+#' #x <-
+#' #  w$nemofy(
+#' #    format = "db",
+#' #    input_id = "runABC456",
+#' #    dbconn = dbconn
+#' #)
+#' #DBI::dbDisconnect(dbconn)
 #' @export
 Wigits <- R6::R6Class(
   "Wigits",
@@ -42,6 +38,7 @@ Wigits <- R6::R6Class(
         cider = Cider,
         cobalt = Cobalt,
         cuppa = Cuppa,
+        esvee = Esvee,
         isofox = Isofox,
         lilac = Lilac,
         linx = Linx,
@@ -54,7 +51,12 @@ Wigits <- R6::R6Class(
         virusbreakend = Virusbreakend,
         virusinterpreter = Virusinterpreter
       )
-      super$initialize(name = "Wigits", path = path, tools = tools, pkg = c("nemo", "tidywigits"))
+      super$initialize(
+        name = "Wigits",
+        path = path,
+        tools = tools,
+        metapkg = c("nemo", "tidywigits")
+      )
     }
   ) # public end
 )
